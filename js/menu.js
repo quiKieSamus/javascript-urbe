6a28a599fd89e4d9c507b65e69789e36a6dc23d5
= <gonzru01atwork@gmail.com>
2022-09-22 09:31:25 -0400

    eliminado proyectos e imagenes del portafolio ruben y solucione overlapping de las cartas en el quienessomos.html

--- js/menu.js
+++ js/menu.js
@@ -3,4 +3,15 @@ const sideMenu = document.getElementById('sideMenu')
 
 hamburgerBtn.addEventListener('click', () => {
   sideMenu.classList.toggle('open');
+});
+
+hamburgerBtn.addEventListener('click', () => { //evento para esconder o reaparecer cartas del quienessomos.html para que no se sobrepongan encima del menu
+  if (sideMenu.classList.contains('open') || 
+  !(sideMenu.classList.contains('open'))) {
+    cardArray = document.querySelectorAll(".card");
+    console.log("asdasd ");
+    for (let i = 0; i < cardArray.length; i++) {
+      cardArray[i].classList.toggle('hide');
+    }
+  }
 })
