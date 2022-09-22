985001c9432d21525912fb7c46481bfd189f3beb
= <gonzru01atwork@gmail.com>
2022-09-22 08:46:08 -0400

    fixed hamburguer button

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,5 @@
 const hamburgerBtn = document.getElementById('hamburgerBtn');
-
+const sideMenu = document.getElementById('sideMenu')
 
 hamburgerBtn.addEventListener('click', () => {
   sideMenu.classList.toggle('open');
