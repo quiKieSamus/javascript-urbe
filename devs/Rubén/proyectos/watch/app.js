b420ff786307cd88525a4a00c39657846a71e4be
= <gonzru01atwork@gmail.com>
2022-10-14 22:23:18 -0400

    ajustes a portafolio y proyecto del reloj listo

--- devs/Rubén/proyectos/watch/app.js
+++ devs/Rubén/proyectos/watch/app.js
@@ -1,4 +1,7 @@
-const watchArea = document.getElementById("watch");
-let date = new Date();
-
-watchArea.innerHTML = `${date.getHours()}: ${date.getMinutes()} :${date.getSeconds()}`;
+const time = () => {
+    const watchArea = document.getElementById("watch");
+    let date = new Date();
+    watchArea.innerHTML = `${date.getHours()}: ${date.getMinutes()} :${date.getSeconds()}`;
+    setTimeout(() => {time()}, 1000);
+}
+time()
